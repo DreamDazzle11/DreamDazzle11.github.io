e7a6cb0afc1139905e382512e57532a287088ac7
DreamDazzle11 <163397269+DreamDazzle11@users.noreply.github.com>
2024-03-14 09:48:20 +0100

    New page

---
+++ script.js
@@ -0,0 +1,30 @@
+let carrito = [];
+let total = 0;
+
+function agregarAlCarrito(nombre, precio) {
+    carrito.push({ nombre, precio });
+    total += precio;
+
+    actualizarCarrito();
+}
+
+function vaciarCarrito() {
+    carrito = [];
+    total = 0;
+
+    actualizarCarrito();
+}
+
+function actualizarCarrito() {
+    const listaCarrito = document.getElementById('lista-carrito');
+    const totalCarrito = document.getElementById('total-carrito');
+
+    listaCarrito.innerHTML = '';
+    carrito.forEach(item => {
+        const li = document.createElement('li');
+        li.textContent = `${item.nombre} - $${item.precio}`;
+        listaCarrito.appendChild(li);
+    });
+
+    totalCarrito.textContent = total;
+}
